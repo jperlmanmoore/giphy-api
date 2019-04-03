01f1776cd5a751df7ef11b032c32bc4e362f4d7c
jperlmanmoore <jperlmanmoore@gmail.com>
2019-04-03 12:17:26 -0400

    buttons appear

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,70 +2,182 @@
 
 $(document).ready(function () {
 
-//array of subjects
-let topics = ["yaaas", "clap", "excited", "love", "no", "yes", "OMG", "LOL", "wow", "Yes", "disgusted", "sad", "tired", "SMH"]; 
+  //initial array of subjects
+  let topics = ["yaaas", "clap", "excited", "love", "no", "yes", "OMG", "LOL", "wow", "yes", "disgusted", "sad", "tired", "SMH"];
 
+  console.log(topics);
 
-//display each button for item in topics array
-$.each(topics, function(i){
-  const buttonDiv = $("<div>");
+  //display each button for item in topics array
 
-  //$("<div>").attr("topicBtns")
+  function renderButtons() {
 
-  const btntext = $("<button>").text(topics[i]).addClass("btn btn-group btn-light btn-group-lg");
+    $("#topicBtns").empty();
 
-  buttonDiv.append(btntext);
+    $.each(topics, function(i) {
 
-  $("#topicBtns").append(buttonDiv);
+      const buttonDiv = $("<div>");
 
-  console.log(btntext);
+      const btnText = $("<button>").text(topics[i]).addClass("text float-left mx-2 my-2 btn btn-group btn-light btn-outline-dark btn-lg");
 
-});
+      buttonDiv.append(btnText);
 
-//function to add searched words to the topics array
+      $("#topicBtns").append(buttonDiv);
 
-function addTopics() {
+      console.log(buttonDiv);
 
-}
-  
- 
-});
-  
-    //   $("#submit").on("click", function () {
+    }); //end for loop for topics
 
-    //       var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ&tag=t-rex&rating=PG&limit=5";
+    console.log(renderButtons);
+  };
 
-    //       var submit = $(this).attr("data-rex");
+  renderButtons();
 
+  $("#inputBtn").on("click", function (e) {
+    e.preventDefault();
 
-    //       $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //       }).then(function (response) {
     
-    //         console.log(response);
 
-    //         var results = response.data;
+    const inputNewTopic = $("#input").val().trim();
 
-    //         jQuery.each(results.length, function (i, val) {
+    topics.push(inputNewTopic);
 
-              
-    //           var imgDiv = $("<button>");
+    renderButtons();
+    
+  });
 
-    //           var image = results[i].
+  //API request -- needs to do two things
+  // const queryURL = "https://api.giphy.com/v1/gifs/random?" +
+  //     search + "api_key=551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ&rating=PG&limit=10";
 
-    //       //$("#btn" + i).append(buttons = element with text)
-    //       //$("img" + i).append(images = element with gifs)
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   }).then(function (response) {
 
-    //   }) //end of loop
+  //     console.log(response);
+
+  //     const results = response.data;
+
+  //     displayGif();
+
+  //   }); //end of .then
+
+  //   //for each result make a gif image appear in the gifDiv
+  // function displayGif() {
+  //   $.each(results, function (i) {
+
+  //     const gifDiv = $("<div>");
+
+  //     const gif = results[i].gif;
+
+  //     gif.attr("src", results[i].gif.fixed_height.url);
+
+  //     gifDiv.append(gif);
+
+  //   });
 
 
-    //   }); //end of .then
 
-    //     //funciton to click to start gif
 
-    //     //funciton to click to stop gif
 
-    //   }); //end submit on click
 
-    // }); //end document ready
+
+  //      $("#add-to-do").on("click", function(event) {
+  // event.preventDefault();
+
+  // // Get the to-do "value" from the textbox and store it a variable
+  // var toDoTask = $("#to-do").val().trim();
+
+  // // Create a new variable that will hold a "<p>" tag.
+  // // Then give it an ID in the following form:
+  // // "item-4" or "item-3" or "item-99", where the number is equal to toDoCount.
+  // // Then set the to-do "value" as text to this <p> element.
+  // var toDoItem = $("<p>");
+
+  // toDoItem.attr("id", "item-" + toDoCount);
+  // toDoItem.text(toDoTask);
+
+  // // Create a button with unique identifiers based on what number it is in the list. Again use jQuery to do this.
+  // // Give your button a data attribute called data-to-do and a class called "checkbox".
+  // // Lastly add a checkmark inside.
+
+  // var toDoClose = $("<button>");
+
+  // toDoClose.attr("data-to-do", toDoCount);
+  // toDoClose.addClass("checkbox");
+  // toDoClose.text("✓");
+
+  // // Append the button to the to do item
+  // toDoItem = toDoItem.prepend(toDoClose);
+
+  // // Add the button and to do item to the to-dos div
+  // $("#to-dos").append(toDoItem);
+
+  // // Clear the textbox when done
+  // $("#to-do").val("");
+
+  //  for the search term make a button
+  // $.each(results, function(i) {
+
+  //    const newTopic[]; 
+
+  //   $.merge($.merge([], first), second);
+
+  // });
+
+
+
+
+
+
+
+
+
+  // var first = [ "a", "b", "c" ];
+  // var second = [ "d", "e", "f" ];
+  // $.merge( $.merge( [], first ), second );
+
+
+  // $.each(results, function(i) {
+  //   list.push(this.data.innerHTML)
+  // });
+
+
+  //   var results = response.data;
+
+  //   jQuery.each(results.length, function (i, val) {
+
+  //   var gifDiv = $("<img>");
+
+  //   gifDiv.attr("src", results[i]);
+
+  //   gifDiv.append()
+
+  //   var gif = results[i].
+
+  // //       //$("#btn" + i).append(buttons = element with text)
+  // //       //$("img" + i).append(images = element with gifs)
+
+
+
+
+
+
+  //stop and start gif
+  $(".gif").on("click", function () {
+    //   var state = $(this).attr("data-state");
+
+    //   if (state === "still") {
+    //     $(this).attr("src", $(this).attr("data-animate"));
+    //     $(this).attr("data-state", "animate");
+    //   } else {
+    //     $(this).attr("src", $(this).attr("data-still"));
+    //     $(this).attr("data-state", "still");
+    //   }
+
+
+    // });
+    //};
+  });
+
+});
