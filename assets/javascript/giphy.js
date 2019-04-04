6ae8e94b4472c5184a03fc0e5a36646ac613757c
jperlmanmoore <jperlmanmoore@gmail.com>
2019-04-04 16:56:47 -0400

    still adding multiple sets  after first click

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
 
       const buttonDiv = $("<div>");
 
-      const btnText = $("<button>").text(topics[i]).addClass("text float-left mx-2 my-2 btn btn-group btn-light btn-outline-dark btn-lg");
-      
+      const btnText = $("<button>").text(topics[i]).addClass("text btncolor float-left mx-2 my-2 btn btn-outline-dark btn-lg");
+
       buttonDiv.append(btnText);
 
       $("#topicBtns").append(buttonDiv);
@@ -30,33 +30,43 @@ $(document).ready(function () {
     // console.log(renderButtons);
   };
 
-  //search box and make a new button
   renderButtons();
+  giphySearch();
 
-  $("#inputBtn").on("click", function (e) {
+  //search box and make a new button
+  function newButton() {
+    $("#inputBtn").on("click", function (e) {
+      e.preventDefault();
+
+      const inputNewTopic = $("#input").val().trim();
+
+      topics.push(inputNewTopic);
+
+      renderButtons();
+    });
+  };
+
+  //new button giphy search
+  $(newButton).on("click", function (e) {
     e.preventDefault();
-
-    const inputNewTopic = $("#input").val().trim();
-
-    topics.push(inputNewTopic);
-
-    renderButtons();
-
-  });
+    giphySearch();
+  })
 
 
-  //
-  $("button").on("click", function (e) {
+  //giphy search on API
+  function giphySearch() {
 
-    e.preventDefault();
+    $(".text").on("click", function (e) {
 
-    const giphy = $(this).text();
+      e.preventDefault();
 
-    const api_key = ("551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ");
+      const giphy = $(this).text();
 
-    const queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + giphy + "&limit=10&api_key=551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ");
+      // const api_key = ("551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ");
 
-    $.ajax({
+      const queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + giphy + "&limit=10&api_key=551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ");
+
+      $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function (response) {
@@ -65,43 +75,47 @@ $(document).ready(function () {
 
         var results = response.data;
 
+        //loop results and display gif
+        $.each(results, function(i) {
 
-        $.each(results, function (i) {
+          // if(results[i] >= 10) {
+          //   return false;
+          // }
 
           const gifDiv = $("<div>");
-
-          const gif = results[i].images;
 
           let displayGif = $("<img>");
           //console.log(displayGif);
 
           displayGif.attr("src", results[i].images.fixed_height.url);
+          displayGif.addClass("mx-2 my-2 float-left align-items-center ");
 
           //gifDiv.append(gif)
           gifDiv.html(displayGif);
 
           $("#gifs").prepend(gifDiv);
 
+          // if (giphySearch() === 10) {
+          //   return false;
+          // }
 
         });
 
-
-
       }); //end of then
-  }); //end of on click inputbtn
+    }); //end of on click inputbtn
+  };
+
+//stop and start gif
+// $(".gif").on("click", function () {
+//     var state = $(this).attr("data-state");
+
+//     if (state === "still") {
+//       $(this).attr("src", $(this).attr("data-animate"));
+//       $(this).attr("data-state", "animate");
+//     } else {
+//       $(this).attr("src", $(this).attr("data-still"));
+//       $(this).attr("data-state", "still");
+//     };
+
 });
 
-
-
-
-        //stop and start gif
-        // $(".gif").on("click", function () {
-        //     var state = $(this).attr("data-state");
-
-        //     if (state === "still") {
-        //       $(this).attr("src", $(this).attr("data-animate"));
-        //       $(this).attr("data-state", "animate");
-        //     } else {
-        //       $(this).attr("src", $(this).attr("data-still"));
-        //       $(this).attr("data-state", "still");
-        //     };
