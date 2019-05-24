ae595f4f63bf75b888d3c5b076a3891eeeee118e
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-05-23 21:07:41 -0400

    changes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,126 +1,126 @@
-$(function () {
-  //initial array of subjects
-  let topics = ["YAAASSS", "YOLO", "WHAAA", "WHAT THE WHAT", "SRSLY", "OMG", "LOL", "143", "GR8", "IKR", "EYEROLL", "SMH", "OKURR", "ERMAHGERD"];
+$(function() {
+    //initial array of subjects
+    let topics = ["YAAASSS", "YOLO", "WHAAA", "WHAT THE WHAT", "SRSLY", "OMG", "LOL", "143", "GR8", "IKR", "EYEROLL", "SMH", "OKURR", "ERMAHGERD"];
 
-  console.log(topics);
+    console.log(topics);
 
-  //display each button for item in topics array
+    //display each button for item in topics array
 
-  function showButtons() {
+    function showButtons() {
 
-    $("#topicBtns").empty();
+        $("#topicBtns").empty();
 
-    $.each(topics, function (i) {
+        $.each(topics, function(i) {
 
-      const buttonDiv = $("<div>");
+            const buttonDiv = $("<div>");
 
-      const btnText = $("<button>").text(topics[i]).addClass("text border-0 float-left mx-2 my-2 btn btn-outline-secondary btn-lg");
+            const btnText = $("<button>").text(topics[i]).addClass("text border-0 float-left mx-2 my-2 btn btn-outline-secondary btn-lg");
 
-      buttonDiv.append(btnText);
+            buttonDiv.append(btnText);
 
-      $("#topicBtns").append(buttonDiv);
+            $("#topicBtns").append(buttonDiv);
 
-      console.log(buttonDiv);
+            console.log(buttonDiv);
 
-    }); //end for loop for topics
+        }); //end for loop for topics
 
-  }; //end showButtons
+    }; //end showButtons
 
-  showButtons();
-  giphySearch();
-  newButton();
+    showButtons();
+    giphySearch();
+    newButton();
 
-  //search box and make a new button
-  //if no text, then no new button
-  //*need to make so clears text on submit
-  //*need to make input text be capital letters
-  function newButton() {
-    $("#inputBtn").on("click", function (e) {
+    //search box and make a new button
+    //if no text, then no new button
+    //*need to make so clears text on submit
+    //*need to make input text be capital letters
+    function newButton() {
+        $("#inputBtn").on("click", function(e) {
 
-      e.preventDefault();
+            e.preventDefault();
 
-      const inputNewTopic = $("#input").val().trim();
+            const inputNewTopic = $("#input").val().trim();
 
-      if (inputNewTopic != "") {   //if inputNewTopis isn't empty
+            if (inputNewTopic != "") { //if inputNewTopis isn't empty
 
-        topics.push(inputNewTopic);
+                topics.push(inputNewTopic);
 
-        showButtons();
+                showButtons();
 
-        giphySearch();
-      } //end of if
-    }); //end of onclick
-  }; //end of newButton
+                giphySearch();
+            } //end of if
+        }); //end of onclick
+    }; //end of newButton
 
-  let results;
+    let results;
 
-  //giphy search on API & display giphys
-  function giphySearch() {
+    //giphy search on API & display giphys
+    function giphySearch() {
 
-    $(".text").on("click", function (e) {
+        $(".text").on("click", function(e) {
 
-      e.preventDefault();
+            e.preventDefault();
 
-      const giphy = $(this).text();
+            const giphy = $(this).text();
 
-      const queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + giphy + "&rating=PG&limit=10&offset=" + Math.floor(Math.random() * 50) + "&api_key=551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ");
+            const queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + giphy + "&rating=PG&limit=10&offset=" + Math.floor(Math.random() * 50) + "&api_key=551UgcDQTs1GvIWT37yUtpo3MbqJ6ShZ");
 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
 
-        console.log(response);
+                console.log(response);
 
-        results = response.data;
+                results = response.data;
 
-        //loop results and display gif
-        $.each(results, function (i) {
+                //loop results and display gif
+                $.each(results, function(i) {
 
-          const gifDiv = $("<div class='gifDiv'>");
+                    const gifDiv = $("<div class='gifDiv'>");
 
-          let displayGif = $("<img>")
+                    let displayGif = $("<img>")
+                        .attr("src", results[i].images.fixed_height_still.url)
+                        .addClass("stopstart mx-2 my-2 float-left align-items-center")
+                        .data("i", i);
+
+                    //console.log(displayGif);
+                    //console.log(movingGif);
+
+                    gifDiv.html(displayGif);
+
+                    $("#gifs").prepend(gifDiv);
+
+                }); //end of each
+            }); //end of then
+        }); //end of on click inputbtn
+
+    };
+
+    //stop and start - more or less from class example 
+    $("#gifs").on("click", ".stopstart", function() {
+
+        //console.log(".stopstart");
+        var i = $(this).data("i");
+        let movingGif = $("<img>")
+            .attr("src", results[i].images.fixed_height.url)
+            .addClass("stopstart moving mx-2 my-2 float-left align-items-center")
+            .data("i", i);
+
+        //still image
+        let displayGif = $("<img>")
             .attr("src", results[i].images.fixed_height_still.url)
             .addClass("stopstart mx-2 my-2 float-left align-items-center")
             .data("i", i);
 
-          //console.log(displayGif);
-          //console.log(movingGif);
+        //moving image
+        let isMoving = $(this).hasClass("moving");
+        //console.log(isMoving);
 
-          gifDiv.html(displayGif);
-
-          $("#gifs").prepend(gifDiv);
-
-        }); //end of each
-      }); //end of then
-    }); //end of on click inputbtn
-
-  };
-
-  //stop and start - more or less from class example 
-  $("#gifs").on("click", ".stopstart", function () {
-
-    //console.log(".stopstart");
-    var i = $(this).data("i");
-    let movingGif = $("<img>")
-      .attr("src", results[i].images.fixed_height.url)
-      .addClass("stopstart moving mx-2 my-2 float-left align-items-center")
-      .data("i", i);
-
-    //still image
-    let displayGif = $("<img>")
-      .attr("src", results[i].images.fixed_height_still.url)
-      .addClass("stopstart mx-2 my-2 float-left align-items-center")
-      .data("i", i);
-
-    //moving image
-    let isMoving = $(this).hasClass("moving");
-    //console.log(isMoving);
-
-    if (isMoving === false) {
-      $(this).replaceWith(movingGif);
-    } else {
-      $(this).replaceWith(displayGif);
-    }; //end else
-  }); //end onclick
+        if (isMoving === false) {
+            $(this).replaceWith(movingGif);
+        } else {
+            $(this).replaceWith(displayGif);
+        }; //end else
+    }); //end onclick
 }); //end document
